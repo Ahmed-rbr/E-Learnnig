5ff0cb795939d1d445ee64748ec10e2ad5fbac89
rabar <ahmedrbr01@gmail.com>
2025-04-03 23:57:10 +0000

    add heder: hero section ,navbar

---
+++ src/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="output.css">
+  </head>
+  <body class="bg-gray-200">
+    <header class="font-mono bg-indigo-500 text-lg">
+<nav class="w-11/12 fixed mb-2  text-white text-lg font-semibold m-auto md:px-6 py-4 flex items-center justify-between">
+<h1 class="flex "><img src="img/logo.svg" alt=""><span class="text-3xl">Educ</span></h1>
+<ul class="flex gap-12 justify-between">
+<li class="hover:text-yellow-300 transform hover:scale-115 transition-all duration-200 ease-in-out "><a href="#">Home</a></li>
+<li class="hover:text-yellow-300 transform hover:scale-115 transition-all duration-200 ease-in-out "><a href="#">Courses</a></li>
+<li class="hover:text-yellow-300 transform hover:scale-115 transition-all duration-200 ease-in-out  "><a href="#">Mentores</a></li>
+<li class="hover:text-yellow-300 transform hover:scale-115 transition-all duration-200 ease-in-out "><a href="#">About</a></li>
+<li class="hover:text-yellow-300 transform hover:scale-115 transition-all duration-200 ease-in-out "><a href="#">Contact</a></li>
+
+
+</ul><div class="flex gap-4">
+<button class="hover:bg-yellow-300 hover:text-white transition duration-200 ease-out hover:cursor-pointer   text-base rounded-3xl bg-white text-black px-4 py-2">Sign in</button>
+<button class="hover:bg-white hover:text-black text-base rounded-3xl transition hover:cursor-pointer duration-200 ease-out bg-yellow-300 px-4 py-2">Sign up</button>
+</div>
+</nav>
+
+    
+
+ 
---
+++ src/index.php
@@ -0,0 +1,22 @@
+<?php include 'header.php'; ?>
+<div class="w-11/12 m-auto md:px-6 py-4 flex flex-row items-center justify-between">
+<div class="w-full flex justify-center flex-col md:w-2/5">
+<p class="w-full text-base text-yellow-300 mb-4">START TO SUCCESS</p>
+<h2 class=" text-3xl text-white mb-4">Access To 5000+ Courses
+from 300 Instructors 
+& Institutions</h2>
+<p class="text-xs text-gray-400 mb-4">Various versions have evolved over the years, sometimes by accident,</p>
+<div class="flex gap-2 w-full">
+      <input type="text" placeholder="What do want to learn?"  class="flex-1 px-4 focus:outline-none py-2 bg-white rounded-lg">
+      <button class="p-2 bg-yellow-300 rounded-lg">
+        <img src="img/MagnifyingGlass.svg" alt="Search" class="w-6 h-6">
+      </button>
+    </div>
+</div>  
+<div class="w-full md:w-6/12"> 
+    <img class="w-full h-auto" src="img/Saly-10 (1).svg" alt="E-Learning">
+  </div>
+</div>
+</header>
+<main></main>
+<?php include 'footer.php'; ?>
