4bfa063c50e0ad0f5e15609a593262819f45362d
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-04-05 23:49:26 +0000

    fixed connection issue

--- src/db.php
+++ src/db.php
@@ -6,14 +6,14 @@ define('DB_PWD','');
 define('DB_HOST','localhost');
 define('DB_NAME','e-learning');
 
-$dsn='mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8mb4';
+$dsn='mysql:host='.DB_HOST.';port=3307;dbname='.DB_NAME.';charset=utf8mb4';
 
 try{
 
   $pdo=new PDO($dsn,DB_USER,DB_PWD);
   $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  echo 'conne';
+  echo 'conne';f
 }
 catch(PDOException $e){
   error_log('connection failed'.$e->getMessage());
